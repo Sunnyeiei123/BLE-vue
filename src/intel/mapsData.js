40c41b071084cc6c2e27f7aa70e1076e5a16e271
nattawatWork <natseakoo.work@gmail.com>
2024-07-11 11:40:51 +0700

    add floor 1 in  component

--- src/intel/mapsData.js
+++ src/intel/mapsData.js
@@ -8,13 +8,89 @@ export default [
         ],
         markers: [
             {
-                position: [817, 1766],
-                popupText: 'สถานที่ 1'
+                position: [1298, 846],
+                popupText: 'AP 1'
             },
             {
-                position: [1300, 850],
-                popupText: 'สถานที่ 2'
-            }
+                position: [1243, 966],
+                popupText: 'AP 2'
+            },
+            {
+                position: [1114,1218],
+                popupText: 'AP 3'
+            },
+            {
+                position: [965,1443],
+                popupText: 'AP 4'
+            },
+            {
+                position: [1130,1381],
+                popupText: 'AP 5'
+            },
+            {
+                position: [1122,1625],
+                popupText: 'AP 6'
+            },
+            {
+                position: [1224,1258],
+                popupText: 'AP 7'
+            },
+            {
+                position: [1391,1193],
+                popupText: 'AP 8'
+            },
+            {
+                position: [1353,1340],
+                popupText: 'AP 9'
+            },
+            {
+                position: [1378,1456],
+                popupText: 'AP 10'
+            },
+            {
+                position: [1305,1499],
+                popupText: 'AP 11'
+            },
+            {
+                position: [1299,1608],
+                popupText: 'AP 12'
+            },
+            {
+                position: [783,1609],
+                popupText: 'AP 13'
+            },
+            {
+                position: [593,1618],
+                popupText: 'AP 14'
+            },
+            {
+                position: [963,1747],
+                popupText: 'AP 15'
+            },
+            {
+                position: [983,1893],
+                popupText: 'AP 16'
+            },
+            {
+                position: [827,1872],
+                popupText: 'AP 17'
+            },
+            {
+                position: [695,1731],
+                popupText: 'AP 18'
+            },
+            {
+                position: [478,1744],
+                popupText: 'AP 19'
+            },
+            {
+                position: [575,1916],
+                popupText: 'AP 20'
+            },
+            {
+                position: [371,1909],
+                popupText: 'AP 21'
+            },
         ]
     },
     {
